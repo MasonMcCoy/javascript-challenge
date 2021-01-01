bdbaa9b4f23607641cfed8cb5092b8396359278e
Mason McCoy <mmasonmccoy@gmail.com>
2021-01-01 17:43:19 -0600

    Wrote script to populate HTML table.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,22 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+function addData(tableID) {
+    // References the HTML table
+    let table = document.getElementById("ufo-table");
+  
+    // Appends rows to table
+    for (let i = 0; i < data.length; i++) {
+        let newRow = table.insertRow(-1);
+        let keys = [tableData[i].datetime, tableData[i].city, tableData[i].state, tableData[i].country, tableData[i].shape, tableData[i].durationMinutes, tableData[i].comments];
+        // Appends cells to rows
+        for (var j = 0; j < keys.length; j++) {
+            let newCell = newRow.insertCell(-1);
+            let newText = document.createTextNode(keys[j]);
+            newCell.appendChild(newText);
+        }
+    }
+  }
+ 
+// Calls addData function to populate table
+addData('ufo-table');
