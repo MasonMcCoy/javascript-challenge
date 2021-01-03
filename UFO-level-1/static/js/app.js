3701111af61427615159540ca152ee46dd12dcbf
Mason McCoy <mmasonmccoy@gmail.com>
2021-01-02 18:08:41 -0600

    Wrote logic to filter results, but table does not update.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,7 +1,7 @@
 // from data.js
 var tableData = data;
 
-function addData(tableID) {
+function addData(Data) {
     // References the HTML table
     let table = document.getElementById("ufo-table");
   
@@ -20,3 +20,43 @@ function addData(tableID) {
  
 // Calls addData function to populate table
 addData('ufo-table');
+
+var button = d3.select("#filter-btn");
+
+button.on("click", filterBy);
+
+function filterBy() {
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value").trim();
+    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+    console.log(filteredData);
+
+    if (filteredData.length !== 0) {
+        addData(filteredData);
+    }
+
+    }
+
+// document.getElementById("filter-btn").addEventListener("click", filterBy);
+
+// function filterBy(txtValue) {
+//     var input, filter, table, tr, td, i, txtValue;
+//     input = document.getElementsByClassName("form-control");
+//     filter = input.value;
+//     table = document.getElementsByClassName("table table-striped");
+//     tr = document.getElementsByTagName("tr");
+
+//     for (i = 0; i < tr.length; i++) {
+//         td = tr[i].getElementsByTagName("td")[0];
+//         if (td) {
+//             txtValue = td.textContent || td.innerText;
+//             if (txtValue.indexOf(filter) > -1) {
+//                 tr[i].style.display = "";
+//             } else {
+//                 tr[i].style.display = "none";
+//             }
+//         }
+//     }
+// }
